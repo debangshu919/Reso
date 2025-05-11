bbe29bdf4f094d436390e014dfacccb42510db40
itz-me-debangshu <personal.debangshu444@gmail.com>
2025-05-11 18:56:29 +0530

    feat: Winston logger setup

---
+++ src/utils/logger.ts
@@ -0,0 +1,74 @@
+import path from "node:path"
+import util from "node:util"
+import { createLogger, format, transports } from "winston"
+import type {
+	ConsoleTransportInstance,
+	FileTransportInstance,
+} from "winston/lib/winston/transports"
+
+const consoleLogFormat = format.printf((info) => {
+	const { level, message, timestamp, meta = {} } = info
+
+	const customLevel = level.toUpperCase()
+	const customTimestamp = timestamp
+	const customMessage = message
+	const customMeta = util.inspect(meta, {
+		showHidden: false,
+		depth: null,
+		colors: true,
+	})
+
+	const customLog = `${customTimestamp} [${customLevel}] ${customMessage}\nMETA: ${customMeta}\n`
+
+	return customLog
+})
+
+const fileLogFormat = format.printf((info) => {
+	const { level, message, timestamp, meta = {} } = info
+
+	const logMeta: Record<string, unknown> = {}
+	for (const [key, value] of Object.entries(meta as object)) {
+		if (value instanceof Error) {
+			logMeta[key] = {
+				name: value.name,
+				message: value.message,
+				trace: value.stack || "",
+			}
+		} else {
+			logMeta[key] = value
+		}
+	}
+
+	const logData = {
+		level: level.toUpperCase(),
+		message,
+		timestamp,
+		meta: logMeta,
+	}
+
+	return JSON.stringify(logData, null, 4)
+})
+
+const consoleTransport = (): Array<ConsoleTransportInstance> => {
+	return [
+		new transports.Console({
+			level: "info",
+			format: format.combine(format.timestamp(), consoleLogFormat),
+		}),
+	]
+}
+
+const fileTransport = (): Array<FileTransportInstance> => {
+	return [
+		new transports.File({
+			filename: path.join(__dirname, "../", "../", "logs", "app.log"),
+			level: "info",
+			format: format.combine(format.timestamp(), fileLogFormat),
+		}),
+	]
+}
+
+export default createLogger({
+	defaultMeta: {},
+	transports: [...fileTransport(), ...consoleTransport()],
+})
