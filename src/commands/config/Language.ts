c66579fdcb1fd4c9afb478923009ca5ab6cdcb91
itz-me-debangshu <personal.debangshu444@gmail.com>
2025-05-22 14:25:29 +0530

    feat: Config commands setup

---
+++ src/commands/config/Language.ts
@@ -0,0 +1,170 @@
+import type { AutocompleteInteraction } from "discord.js"
+import { Command, type Context, type Reso } from "../../classes/index"
+import { LocaleFlags } from "../../constants/LOCALE_FLAGS"
+import { env } from "../../env"
+import { Language } from "../../types/Language"
+
+export default class LanguageCommand extends Command {
+	constructor(client: Reso) {
+		super(client, {
+			name: "language",
+			description: {
+				content: "cmd.language.description",
+				examples: ["language set `EnglishUS`", "language reset"],
+				usage: "language",
+			},
+			category: "config",
+			aliases: ["lang"],
+			cooldown: 3,
+			args: true,
+			vote: false,
+			player: {
+				voice: false,
+				dj: false,
+				active: false,
+				djPerm: null,
+			},
+			permissions: {
+				dev: false,
+				client: [
+					"SendMessages",
+					"ReadMessageHistory",
+					"ViewChannel",
+					"EmbedLinks",
+				],
+				user: ["ManageGuild"],
+			},
+			slashCommand: true,
+			options: [
+				{
+					name: "set",
+					description: "cmd.language.options.set",
+					type: 1,
+					options: [
+						{
+							name: "language",
+							description: "cmd.language.options.language",
+							type: 3,
+							required: true,
+							autocomplete: true,
+						},
+					],
+				},
+				{
+					name: "reset",
+					description: "cmd.language.options.reset",
+					type: 1,
+				},
+			],
+		})
+	}
+
+	public async run(client: Reso, ctx: Context, args: string[]): Promise<any> {
+		let subCommand: string | undefined
+
+		if (ctx.isInteraction) {
+			subCommand = ctx.options.getSubCommand()
+		} else {
+			subCommand = args.shift()
+		}
+
+		const defaultLanguage = env.DEFAULT_LANGUAGE || Language.EnglishUS
+
+		if (subCommand === "set") {
+			const embed = client.embed().setColor(this.client.color.main)
+
+			const locale =
+				(await client.db.getLanguage(ctx.guild!.id)) || defaultLanguage
+
+			let lang: string
+
+			if (ctx.isInteraction) {
+				lang = ctx.options.get("language")?.value as string
+			} else {
+				lang = args[0]
+			}
+
+			if (!Object.values(Language).includes(lang as Language)) {
+				const availableLanguages = Object.entries(LocaleFlags)
+					.map(([key, value]) => `${value}:\`${key}\``)
+					.reduce((acc, curr, index) => {
+						if (index % 2 === 0) {
+							return (
+								acc +
+								curr +
+								(index === Object.entries(LocaleFlags).length - 1 ? "" : " ")
+							)
+						}
+						return `${acc + curr}\n`
+					}, "")
+				return ctx.sendMessage({
+					embeds: [
+						embed.setDescription(
+							ctx.locale("cmd.language.invalid_language", {
+								languages: availableLanguages,
+							}),
+						),
+					],
+				})
+			}
+
+			if (locale && locale === lang) {
+				return ctx.sendMessage({
+					embeds: [
+						embed.setDescription(
+							ctx.locale("cmd.language.already_set", {
+								language: lang,
+							}),
+						),
+					],
+				})
+			}
+
+			await client.db.updateLanguage(ctx.guild!.id, lang)
+			ctx.guildLocale = lang
+
+			return ctx.sendMessage({
+				embeds: [
+					embed.setDescription(
+						ctx.locale("cmd.language.set", { language: lang }),
+					),
+				],
+			})
+		}
+		if (subCommand === "reset") {
+			const embed = client.embed().setColor(this.client.color.main)
+
+			const locale = await client.db.getLanguage(ctx.guild!.id)
+
+			if (!locale) {
+				return ctx.sendMessage({
+					embeds: [embed.setDescription(ctx.locale("cmd.language.not_set"))],
+				})
+			}
+
+			await client.db.updateLanguage(ctx.guild!.id, defaultLanguage)
+			ctx.guildLocale = defaultLanguage
+
+			return ctx.sendMessage({
+				embeds: [embed.setDescription(ctx.locale("cmd.language.reset"))],
+			})
+		}
+	}
+
+	public async autocomplete(
+		interaction: AutocompleteInteraction,
+	): Promise<void> {
+		const focusedValue = interaction.options.getFocused()
+
+		const languages = Object.values(Language).map((language) => ({
+			name: language,
+			value: language,
+		}))
+
+		const filtered = languages.filter((language) =>
+			language.name.toLowerCase().includes(focusedValue.toLowerCase()),
+		)
+
+		await interaction.respond(filtered.slice(0, 25)).catch(console.error)
+	}
+}
